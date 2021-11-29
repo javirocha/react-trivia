36e856303ad97bdebc96cb4d97c5023849db7ca3
Javier Rocha <hola@javirocha.com>
2021-11-29 15:23:13 -0300

    Adds redirect to homepage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useSelector } from 'react-redux'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Game from './components/Game'
 import Scores from './components/Scores'
 
@@ -11,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/*" element={ <Game /> } />
         <Route path="scores" element={ <Scores /> } />
+        <Route path="*" element={ <Navigate to="/" /> } />
       </Routes>
     </BrowserRouter>
   );
